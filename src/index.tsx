9d850b046a02c8b9b3b7d6916fc35d32abb90adb
flynn-eye <939649107@qq.com>
2020-11-10 20:44:16 +0800

    test(test_ci/cd): test ci/cd

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import App from './App';
 import Store from './store/index';
 import 'normalize.css/normalize.css';
+import 'antd/dist/antd.css';
 ReactDOM.render(
   <Provider store={Store()}>
     <App />
