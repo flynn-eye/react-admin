36cfb28e08b707ac767c01117c3ab1c2d74cf86d
flynn-eye <939649107@qq.com>
2020-11-29 23:11:51 +0800

    feat(pages): 优化对移动端的支持

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,37 +1,62 @@
-import React from 'react';
+import React, { useState, Dispatch, SetStateAction } from 'react';
 import './index.scss';
 import { Row, Col, Dropdown, Menu } from 'antd';
-import { TranslationOutlined, BellOutlined, QuestionCircleOutlined } from '@ant-design/icons';
+import {
+  TranslationOutlined,
+  BellOutlined,
+  QuestionCircleOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined
+} from '@ant-design/icons';
 import avatar from '../../asserts/header.png';
 import HeaderSearch from '../headerSearch';
-const Header = (props: any) => {
-  console.log(props);
+import logo from '../../asserts/logo.svg';
+interface IProps {
+  isMenuOpen: boolean;
+  setIsMenuOpen: Dispatch<SetStateAction<boolean>>;
+}
+const Header = (props: IProps) => {
+  const { isMenuOpen, setIsMenuOpen } = props;
+  let OpenIcon = null;
+  const openMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  if (isMenuOpen) {
+    OpenIcon = <MenuFoldOutlined onClick={openMenu} />;
+  } else {
+    OpenIcon = <MenuUnfoldOutlined onClick={openMenu} />;
+  }
   return (
-    <Row wrap={false} className="global__container" align="middle" justify="space-between">
-      <Col flex="1 1 0%">left</Col>
-      <Col>
-        <HeaderSearch />
-      </Col>
-      <Col className="icon">
-        <QuestionCircleOutlined />
-      </Col>
-      <Dropdown overlay={MsgMenu} placement="bottomLeft">
+    <>
+      <Row wrap={false} className="global__container" align="middle" justify="space-between">
+        <Col flex="1 1 0%">
+          <img className="logo" src={logo} />
+          {OpenIcon}
+        </Col>
+        <Col>
+          <HeaderSearch />
+        </Col>
         <Col className="icon">
-          <BellOutlined />
+          <QuestionCircleOutlined />
         </Col>
-      </Dropdown>
-      <Dropdown overlay={AvatarMenu} placement="bottomCenter">
-        <Col className="userProfile">
-          <img className="avatar" src={avatar} />
-          <span className="name">flynn-eye</span>
-        </Col>
-      </Dropdown>
-      <Dropdown overlay={LangMenu} placement="bottomLeft">
-        <Col className="icon">
-          <TranslationOutlined />
-        </Col>
-      </Dropdown>
-    </Row>
+        <Dropdown overlay={MsgMenu} placement="bottomLeft">
+          <Col className="icon">
+            <BellOutlined />
+          </Col>
+        </Dropdown>
+        <Dropdown overlay={AvatarMenu} placement="bottomCenter">
+          <Col className="userProfile">
+            <img className="avatar" src={avatar} />
+            <span className="name">flynn-eye</span>
+          </Col>
+        </Dropdown>
+        <Dropdown overlay={LangMenu} placement="bottomLeft">
+          <Col className="icon">
+            <TranslationOutlined />
+          </Col>
+        </Dropdown>
+      </Row>
+    </>
   );
 };
 const AvatarMenu = () => {
--- src/layout/menuLayout/leftMenuLayout/index.tsx
+++ src/layout/menuLayout/leftMenuLayout/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useState } from 'react';
 import './index.scss';
-import { Layout, Row } from 'antd';
+import { Layout, Row, Drawer } from 'antd';
 import { IConfig } from '../../../util/config';
 import FooterContent from '../../../components/footer';
 import Menu from '../../../components/menu';
@@ -11,7 +11,8 @@ interface ITitleStyle {
   display: string;
 }
 const LeftMenuLayout: FC<IConfig> = (props) => {
-  const { layoutMode, fixHeader, theme, language, showFooter, children } = props;
+  const { layoutMode, fixHeader, theme, language } = props;
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   let [titleStyle, setTitleStyle] = useState({
     display: 'inline-block'
   });
@@ -26,32 +27,63 @@ const LeftMenuLayout: FC<IConfig> = (props) => {
       });
     }
   };
-  return (
-    <Layout className="LeftMenuLayout__container">
-      <Sider
-        onCollapse={() => changeTitleStatus()}
-        breakpoint="md"
-        collapsible={true}
-        className="sider"
-      >
+  const XSider = (
+    <Sider
+      onCollapse={() => changeTitleStatus()}
+      breakpoint="md"
+      collapsible={true}
+      className="siderShow sider"
+    >
+      <Row justify="center" align="middle" className="sider__logo">
+        <a>
+          <img src={logo} alt="" />
+          <h1 style={titleStyle}>ant design pro</h1>
+        </a>
+      </Row>
+      <Menu language={language} theme={theme} layoutMode={layoutMode} fixHeader={fixHeader} />
+    </Sider>
+  );
+  const DrawerSider = (
+    <div className="LeftMenuLayout__container">
+      <Sider onCollapse={() => changeTitleStatus()} collapsible={true} className="sider">
         <Row justify="center" align="middle" className="sider__logo">
           <a>
             <img src={logo} alt="" />
-            <h1 style={titleStyle}>ant design pro</h1>
+            <h1>ant design pro</h1>
           </a>
         </Row>
         <Menu language={language} theme={theme} layoutMode={layoutMode} fixHeader={fixHeader} />
       </Sider>
-      <Layout>
-        <Header className="header">
-          <GlobalHeader />
-        </Header>
-        <Content>Content</Content>
-        <Footer>
-          <FooterContent />
-        </Footer>
+    </div>
+  );
+  return (
+    <>
+      <Layout className="LeftMenuLayout__container">
+        {XSider}
+        <Layout>
+          <Header className="header">
+            <GlobalHeader isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+          </Header>
+          <Content>Content</Content>
+          <Footer>
+            <FooterContent />
+          </Footer>
+        </Layout>
       </Layout>
-    </Layout>
+      <Drawer
+        headerStyle={{ display: 'none' }}
+        placement="left"
+        closable={false}
+        onClose={() => {
+          setIsMenuOpen(false);
+        }}
+        visible={isMenuOpen}
+        width="200"
+        bodyStyle={{ padding: 0 }}
+      >
+        {DrawerSider}
+      </Drawer>
+    </>
   );
 };
 export default LeftMenuLayout;
